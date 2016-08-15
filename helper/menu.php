975073c42a1393d50921e25c337c5d1034d09dc5
Leo Baiano <ljunior2005@gmail.com>
2016-08-15 16:36:10 -0300

    padroniza o codigo da helper menu

--- helper/menu.php
+++ helper/menu.php
@@ -22,16 +22,15 @@ class LB_Menu_Splinter {
 	private $slugs;
 
 	public function __construct( $name, $slugs ) {
-		$this->name = $name;
-		$this->slugs = $slugs;
+		$this->name 	= $name;
+		$this->slugs 	= $slugs;
 		$this->custom_menu();
 	}
 
 	public function custom_menu() {
 		add_menu_page( $this->name, $this->name, 'edit_posts', 'edit.php?post_type='. $this->slugs[0]['slug'], '' );
-
-	    	foreach ($this->slugs as $key => $value) {
-	    		add_submenu_page( 'edit.php?post_type='. $this->slugs[0]['slug'], $value['name'], $value['name'], 'edit_posts', 'edit.php?post_type='. 	$value['slug'], '' );
-	    	}
+		foreach ( $this->slugs as $key => $value ) {
+			add_submenu_page( 'edit.php?post_type='. $this->slugs[0]['slug'], $value['name'], $value['name'], 'edit_posts', 'edit.php?post_type='. 	$value['slug'], '' );
+		}
 	}
 }
